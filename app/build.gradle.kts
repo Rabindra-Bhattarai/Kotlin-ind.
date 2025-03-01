fac0b14cb32ab68468ba056f52210b6b7f14af7e
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2025-03-01 18:40:04 +0545

    Instrumented Testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,8 @@ dependencies {
     testImplementation("org.mockito:mockito-core:5.6.0")
     testImplementation("org.mockito:mockito-inline:3.12.4")
     androidTestImplementation("org.mockito:mockito-kotlin:3.2.0")
-
+    androidTestImplementation("androidx.test:runner:1.2.0")
+    androidTestImplementation("androidx.test:rules:1.2.0")
 
 
 }
